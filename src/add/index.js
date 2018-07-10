49a0fe5d3222440a857d7c8ea2842b678f83f500
Siva <siva.durgarao@synchronoss.com>
2018-07-10 16:19:28 +0530

    Modal component with functions usage in react components

--- src/add/index.js
+++ src/add/index.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import addActions from './actions.js'
-import { Alert } from 'react-bootstrap';
+import { Alert, Modal, Button } from 'react-bootstrap';
 
 class addItem extends Component{
-    state = { itemName: '' };
+    constructor(props, context){
+        super(props, context);
+        this.state = { itemName: '', statusMessage: '' };
+        this.handleStatusClose = this.handleStatusClose.bind(this);
+    }
+
+    handleStatusClose() {
+        this.setState({statusMessage: ''});
+    }
+
     render(){
         let { add } = this.props;
         return (
@@ -15,8 +24,19 @@ class addItem extends Component{
                         {this.state.statusMessage}
                     </Alert>
                 }
+
+                <Modal show={this.state.statusMessage !== ''}>
+                    <Modal.Header closeButton onHide={this.handleStatusClose}>
+                        <Modal.Title>Add Action Status</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <p>{this.state.statusMessage}</p>
+                    </Modal.Body>
+                    <Modal.Footer><Button onClick={this.handleStatusClose}>Close</Button></Modal.Footer>
+                </Modal>
+
                 <h4>Add New Item:</h4>
-                <input name="item-name" value={this.state.itemName} onChange={(evt) => this.setState({itemName: evt.target.value, statusMessage: false}) } />
+                <input name="item-name" value={this.state.itemName} onChange={(evt) => this.setState({itemName: evt.target.value, statusMessage: ''}) } />
                 <button type="button" onClick={() => {add(this.state.itemName).then( (response) => { console.log(response); this.setState({ statusMessage: 'New item has been added successfully.' }) })}}>ADD</button>
             </section>
         )
